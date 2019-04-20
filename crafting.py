62a610c919d9d0cccb216e38d4d5af41965b4443
Collector <gilad.david95@gmail.com>
2019-04-20 12:31:04 +0300

    Split crafting functions

--- crafting.py
+++ crafting.py
@@ -3,6 +3,16 @@ import save
 items = save.load_items()
 
 def craft_item(item_name, quantity):
+    requirements = get_requirements(item_name, quantity)
+    print(requirements)
+    for quantity, item_name in requirements:
+        try:
+            output[item_name] += quantity
+        except KeyError:
+            output[item_name] = quantity
+    return output
+
+def get_requirements(item_name, quantity):
     item = next((item for item in items if item.name == item_name), None)
 
     if item:
@@ -10,13 +20,9 @@ def craft_item(item_name, quantity):
         try:
             for requirement in item.recipe.positions:
                 positions, item_name = requirement
-                re.append(craft_item(item_name, quantity*len(positions)/item.recipe.quantity))
-            return re
+                re.append(get_requirements(item_name, quantity*len(positions)/item.recipe.quantity))
+            return [requirement for requirements in re for requirement in requirements]
         except AttributeError:
-            return [quantity, item.name]
+            return [[quantity, item.name]]
     else:
         return "Item doesn't exist"
-
-
-
-        
