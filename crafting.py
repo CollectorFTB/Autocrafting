97807982e6db7ef890547b6d0141cc71a07e3cd9
Collector <gilad.david95@gmail.com>
2019-04-20 22:52:48 +0300

    Using default dict

--- crafting.py
+++ crafting.py
@@ -1,27 +1,26 @@
 import save
 from item import Item
-from typing import List
-
+from typing import List, Dict
+from more_itertools import flatten
+from itertools import groupby
+from collections import defaultdict,Counter
 items :List[Item] = save.load_items()
 
-def craft_item(item_name, quantity=1):
-    requirements = get_requirements(item_name, quantity)
-    output = dict()
+def craft_item(item_name, quantity=1) -> Dict[str, int]:
+    requirements = list(get_requirements(item_name, quantity))
+    
+    output = defaultdict(int)
     for quantity, item_name in requirements:
-        try:
-            output[item_name] += quantity
-        except KeyError:
-            output[item_name] = quantity
-    print(output)
-    return output
+        output[item_name] += quantity
+    
+    return dict(output)
 
 def get_requirements(item_name:str, quantity:int) -> List[List[Item]]:
     item = next((item for item in items if item.name == item_name), None)
 
     if item:
         if item.recipe:
-            re = [get_requirements(item_name, quantity*len(positions)/item.recipe.quantity) for positions, item_name in item.recipe.positions]
-            return [requirement for requirements in re for requirement in requirements]
+            return flatten([get_requirements(item_name, quantity*len(positions)/item.recipe.quantity) for positions, item_name in item.recipe.positions])
         else:
             return [[quantity, item.name]]
     else:
