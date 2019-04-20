d3b39414cad451aff2731b4d12446e1e587f2fa8
Collector <gilad.david95@gmail.com>
2019-04-20 22:28:37 +0300

    Adjested to new Item class

--- crafting.py
+++ crafting.py
@@ -1,28 +1,29 @@
 import save
+from item import Item
+from typing import List
 
-items = save.load_items()
+items :List[Item] = save.load_items()
 
-def craft_item(item_name, quantity):
+def craft_item(item_name, quantity=1):
     requirements = get_requirements(item_name, quantity)
-    print(requirements)
+    output = dict()
     for quantity, item_name in requirements:
         try:
             output[item_name] += quantity
         except KeyError:
             output[item_name] = quantity
+    print(output)
     return output
 
-def get_requirements(item_name, quantity):
+def get_requirements(item_name:str, quantity:int) -> List[List[Item]]:
     item = next((item for item in items if item.name == item_name), None)
 
     if item:
-        re = list()
-        try:
-            for requirement in item.recipe.positions:
-                positions, item_name = requirement
-                re.append(get_requirements(item_name, quantity*len(positions)/item.recipe.quantity))
+        if item.recipe:
+            re = [get_requirements(item_name, quantity*len(positions)/item.recipe.quantity) for positions, item_name in item.recipe.positions]
             return [requirement for requirements in re for requirement in requirements]
-        except AttributeError:
+        else:
             return [[quantity, item.name]]
     else:
-        return "Item doesn't exist"
+        raise Exception("Item doesn't exist: " + item_name)
+
